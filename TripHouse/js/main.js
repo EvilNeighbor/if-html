312ca44b967dd15d853814471ef816bc6ec2378c
EvilNeighbor <denissycheuski@gmail.com>
2021-06-22 08:57:52 +0300

    Lesson-11 HTML/CSS

--- TripHouse/js/main.js
+++ TripHouse/js/main.js
@@ -49,36 +49,38 @@ $(document).ready(function(){
   });
 });
 
-// $(document).ready(function(){
-//   $('.slick-wrapper').slick({
-// 		slidesToShow: 3 ,
-// 		slidesToScroll: 1,
-// 		infinite: true,
-// 		arrows: true,
-// 		 responsive: [
-//     {
-//       breakpoint: 991,
-//       settings: {
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
+$(document).ready(function(){
+  $('.slick-wrapper').slick({
+		slidesToShow: 3 ,
+		slidesToScroll: 1,
+		infinite: true,
+		arrows: true,
+		 responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
        
-//       }
-//     },
-//     {
-//       breakpoint: 767,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 1,
-//       }
-//     },
-//     {
-//       breakpoint: 575,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 1,
-//       }
-//     }   
-//   ]
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+				arrows: false,
+				dots: true,
+      }
+    }   
+  ]
    
-//   });
-// });
+  });
+});
